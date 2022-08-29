cea81759eda250c19ecea47f66f546bfcbf90c20
Shaheem Naqvi <66822511+ShaheemNaqvi@users.noreply.github.com>
2022-08-29 11:52:24 +0500

    Create connect.js

---
+++ connection/connect.js
@@ -0,0 +1,9 @@
+var elasticsearch = require('elasticsearch')
+
+
+var client = new elasticsearch.Client({
+    hosts: ['http://username:password@192.168.122.42:9200']
+});
+
+
+module.exports = client
