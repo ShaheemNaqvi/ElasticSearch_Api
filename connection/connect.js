96eec2818d448194d14db37f658777edd266b33b
ShaheemNaqvi <shaheemnaqvi@gmail.com>
2022-08-30 11:44:19 +0500

    undici

--- connection/connect.js
+++ connection/connect.js
@@ -2,14 +2,10 @@ var elasticsearch = require('elasticsearch')
 
 
 var client = new elasticsearch.Client({
-    hosts: ['http://username:password@192.168.122.42:9200']
+    hosts: ['http://elastic:deep@123@192.168.122.42:9200']
 });
 
 
-<<<<<<< HEAD
 
 
 module.exports = client
-=======
-module.exports = client
->>>>>>> cea81759eda250c19ecea47f66f546bfcbf90c20
