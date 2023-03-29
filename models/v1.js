9d83fb4d4e1e6d35f9d69bc207b58d699043e1e3
ShaheemNaqvi <shaheemnaqvi@gmail.com>
2023-03-29 12:00:18 +0500

    final

--- models/v1.js
+++ models/v1.js
@@ -7,78 +7,42 @@ const parseElasticResponse = (elasticResponse) => {
     return result;
 };
 
-exports.L7protocol = async (req, res) =>{
-    const searchText = req.query.q;
-    client.search({  
-        index: 'network_stats',
-        body: {
-          query: {
-              wildcard : {
-                  "L7_Proto_name" : {value:"*"}
-                  }
-              },
-              _source: ["L7_Proto_name"],
-              //highlight: {fields : {"Protocol" : {}}},
-              from: 0,
-              size: 300,
-              sort: [],
-              aggs: {}
-          }
-      },function (error, resp,status) {
-          if (error){
-            console.log("search error: "+error)
-            return res.status(400).send({
-                message: `not found`
-           });
-          }
-          else {
-            search= resp;
-            console.log('Found response',search);
-            if(!search){
-                return res.status(400).send({
-                    message: 'search not found for id '
-                });
-            }
-            return res.status(200).json({
-                records: parseElasticResponse(search),
-            });
-          }
-      });
-  };
 
-
-exports.L4protocol = async (req, res) =>{
+exports.L7protocol = async (req, res) => {
     const searchText = req.query.q;
-    client.search({  
-        index: 'network_stats',
+    client.search({
+        index: 'ipdrs',
         body: {
-          query: {
-              wildcard : {
-                  "L4_Proto_name" : {value:"*"}
-                  }
-              },
-              _source: ["L4_Proto_name"],
-              //highlight: {fields : {"Protocol" : {}}},
-              //from: 0,
-              //size: 0,
-              //sort: [],
-              aggs: {
-                L4_Proto_name : {
-                    terms : { field : "L4_Proto_name.keyword",  size : "5" }
+            query: {
+                wildcard: {
+                    "l7_app_proto.keyword": { value: "*" }
                 }
-              }
-          }
-      },function (error, resp,status) {
-          if (error){
-            console.log("search error: "+error)
+            },
+            _source: ["l7_app_proto"],
+            //highlight: {fields : {"Protocol" : {}}},
+            from: 0,
+            //size: 10,
+            sort: [],
+            aggs: {
+                l7_app_proto: {
+                    terms: {
+                      field: 'l7_app_proto.keyword',
+                      size: 10
+                    }
+                }
+            }
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
             return res.status(400).send({
                 message: `not found`
-           });
-          }
-          else {
-            search= resp;
-            console.log('Found response',search);
-            if(!search){
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
                 return res.status(400).send({
                     message: 'search not found for id '
                 });
@@ -86,23 +50,369 @@ exports.L4protocol = async (req, res) =>{
             return res.status(200).json({
                 records: parseElasticResponse(search),
             });
-          }
-      });
-  };
+        }
+    });
+};
 
-exports.protometa = async (req, res) =>{
+exports.L4protocol = async (req, res) => {
+    const searchText = req.query.q;
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                wildcard: {
+                    "L4_Proto_name": { value: "*" }
+                }
+            },
+            _source: ["L4_Proto_name"],
+            //highlight: {fields : {"Protocol" : {}}},
+            //from: 0,
+            //size: 0,
+            sort: [],
+            aggs: {
+                L4_Proto_name: {
+                    terms: { field: 'L4_Proto_name.keyword', size: 5 }
+                }
+            }
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.protometa = async (req, res) => {
     const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["l7_app_proto", "L4_Proto_name"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.sourip = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["Src_IP"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.destip = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["Dst_IP"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.web = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["L7_proto_cat"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.loc = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["source.geo.geo.country_name"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.region = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["source.geo.geo.region_name"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.destloc = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["destination.geo.geo.country_name"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.destcity = async (req, res) => {
+    const searchText = req.query.q
+    client.search({
+        index: 'ipdrs',
+        body: {
+            query: {
+                multi_match: {
+                    query: searchText.trim(),
+                    fields: ["source.geo.geo.city_name"],
+                    type: "phrase_prefix"
+                }
+            },
+            //_source : ["msg"]
+        }
+    }, function (error, resp, status) {
+        if (error) {
+            console.log("search error: " + error)
+            return res.status(400).send({
+                message: `not found`
+            });
+        }
+        else {
+            search = resp;
+            console.log('Found response', search);
+            if (!search) {
+                return res.status(400).send({
+                    message: 'search not found for id '
+                });
+            }
+            return res.status(200).json({
+                records: parseElasticResponse(search),
+            });
+        }
+    });
+};
+
+exports.multisearch = async (req, res) =>{
+    const searchText = req.query.q
+    const gt = req.query.gt
+    const lt = req.query.lt 
     client.search({  
-        index: 'network_stats',
+        index: 'ipdrs',
+        //type: '_doc',
         body: {
           query: {
-            multi_match : {
-                query:    searchText.trim(), 
-                fields: [ "L7_Proto_name","L4_Proto_name"],
-                type : "phrase_prefix" 
+                bool:{
+                    must:{
+                        multi_match : {
+                            query:    searchText.trim(), 
+                            fields: [ "l7_app_proto","L4_Proto_name"],
+                            type : "phrase_prefix" 
+                        }
+                    },
+                    filter:[{
+                        range:{
+                            Timestamp:{
+                                format: "strict_date_optional_time",
+                                gte: gt.trim(),//"2021-10-1",
+                                lte: lt.trim() //"2022-10-19"
+                            },
+                        }
+                    }]
+                },
             }
-          },
-          //_source : ["msg"]
         }
       },function (error, resp,status) {
           if (error){
--- routes/v1.routes.js
+++ routes/v1.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const search= require ('../models/search');
-const v1= require ('../models/v1');
+const search = require('../models/search');
+const v1 = require('../models/v1');
 const router = express.Router();
 
 const parseElasticResponse = (elasticResponse) => {
@@ -25,10 +25,54 @@ router.get('/protocolmeta', async (req, res) => {
     v1.protometa(req, res);
 });
 
+// route to ip
+//v1/sourceip:q=ip
+router.get('/sourceip', async (req, res) => {
+    v1.sourip(req, res);
+});
+
+// route to ip
+//v1/sourceip:q=ip
+router.get('/destip', async (req, res) => {
+    v1.destip(req, res);
+});
+
+// route to category
+//v1/l4category:q=web
+router.get('/l4category', async (req, res) => {
+    v1.web(req, res);
+});
+
+// route to country name
+//v1/sourceloc:q=pakistan
+router.get('/sourceloc', async (req, res) => {
+    v1.loc(req, res);
+});
+
+// route to region
+//v1/sourceregion:q=pakistan
+router.get('/sourceregion', async (req, res) => {
+    v1.region(req, res);
+});
+
+// route to city name
+//v1/sourcecity:q=pakistan
+router.get('/sourcecity', async (req, res) => {
+    v1.city(req, res);
+});
+
+router.get('/destcity', async (req, res) => {
+    v1.destcity(req, res);
+});
+
+
+router.get('/destloc', async (req, res) => {
+    v1.destloc(req, res);
+});
 //Get specific protocol data
 //http://localhost:1214/v1/dpiprobe/protocol?q=YouTubeUpload&gt=2021-10-1&lt=2022-10-20
 router.get('/dpiprobe/protocol', async (req, res) => {
-    search.multisearch3(req, res);
+    v1.multisearch(req, res);
 });
 
-module.exports =router;
+module.exports = router;
